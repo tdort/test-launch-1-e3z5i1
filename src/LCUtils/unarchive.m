f6e55022764ee157d5a7dae81e864c97d6455e71
tdort <denisnenitoilet@gmail.com>
2026-09-23 00:13:09 +0200

    Update unarchive.m

--- src/LCUtils/unarchive.m
+++ src/LCUtils/unarchive.m
@@ -1,3 +1,4 @@
+#import "src/Utils.h"
 #import "src/components/LogUtils.h"
 #import "unarchive.h"
 
@@ -183,8 +184,8 @@ int addFileToArchive(struct archive* write, NSString* filePath, NSString* basePa
 	//"Payload/Cowabunga.app"
 	// relativePath = [@"Payload/GeodeHelper.app" stringByAppendingString:relativePath];
 	relativePath = [@"Payload/GD" stringByAppendingString:relativePath];
-	// relativePath = [relativePath stringByReplacingOccurrencesOfString:@"GDcom.dort.novadashhhhhhh.app" withString:@"GeodeHelper.app"];
-	relativePath = [relativePath stringByReplacingOccurrencesOfString:@"GDcom.dort.novadashhhhhhh.app" withString:@"NovaDash.app"];
+	// relativePath = [relativePath stringByReplacingOccurrencesOfString:[@"GD" stringByAppendingString:[Utils gdBundleName]] withString:@"GeodeHelper.app"];
+	relativePath = [relativePath stringByReplacingOccurrencesOfString:[@"GD" stringByAppendingString:[Utils gdBundleName]] withString:@"NovaDash.app"];
 	archive_entry_set_pathname(entry, relativePath.fileSystemRepresentation);
 
 	NSString* fileType = attributes[NSFileType];
